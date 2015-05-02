cbf32f1ced870cd9109c1dccf4b0cafc8e6669c3
Reut Sharabani <reut.sharabani@gmail.com>
2015-05-02 14:26:31 +0300

    extracted round context to a new object

--- logic/poker/players.py
+++ logic/poker/players.py
@@ -14,12 +14,12 @@ LOGGER.addHandler(stream_handler)
 
 class Action(object):
 
-    def __init__(self, player, game):
+    def __init__(self, player, round_):
         self.player = player
-        self.game = game
+        self.round = round_
 
     @staticmethod
-    def is_valid(player, game):
+    def is_valid(player, round_):
         raise NotImplementedError("Can not validate abstract Action")
 
     def apply(self):
@@ -28,12 +28,12 @@ class Action(object):
 
 class AmountableAction(Action):
 
-    def __init__(self, player, game, amount):
-        super(AmountableAction, self).__init__(player, game)
+    def __init__(self, player, round_, amount):
+        super(AmountableAction, self).__init__(player, round_)
         self.amount = amount
 
     @staticmethod
-    def is_valid(player, game):
+    def is_valid(player, round_):
         raise NotImplementedError("Can not validate abstract Action")
 
     def apply(self):
@@ -43,18 +43,18 @@ class AmountableAction(Action):
 class Fold(Action):
 
     @staticmethod
-    def is_valid(player, game):
+    def is_valid(player, round_):
         return not player.folded
 
     def apply(self):
-        self.game.active_players.remove(self.player)
+        self.round.folded_players.append(self.player)
 
 
 class Check(Action):
 
     @staticmethod
-    def is_valid(player, game):
-        pot = game.pot
+    def is_valid(player, round_):
+        pot = round_.pot
         return pot.player_bet(player) == pot.current_bet
 
     def apply(self):
@@ -63,33 +63,33 @@ class Check(Action):
 
 class Call(Action):
 
-    def __init__(self, player, game):
-        super(Call, self).__init__(player, game)
+    def __init__(self, player, round_):
+        super(Call, self).__init__(player, round_)
 
     @staticmethod
-    def is_valid(player, game):
-        return player.money >= game.pot.amount_to_call(player) > 0
+    def is_valid(player, round_):
+        return player.money >= round_.pot.amount_to_call(player) > 0
 
     def apply(self):
-        self.player.money -= self.game.pot.amount_to_call(self.player)
+        self.player.money -= self.round.pot.amount_to_call(self.player)
 
 
 class Bet(AmountableAction):
 
-    def __init__(self, player, game, amount):
-        bet_min, bet_max = game.pot.minimum_to_bet(player), player.money
+    def __init__(self, player, round_, amount):
+        bet_min, bet_max = round_.pot.minimum_to_bet(player), player.money
         if bet_max < amount < bet_min:
             raise NotEnoughMoneyException("%s can't bet %d - bet has to be between %d to %d!" % (
                 player.name, amount, bet_min, bet_max
             ))
-        super(Bet, self).__init__(player, game, amount)
+        super(Bet, self).__init__(player, round_, amount)
 
     @staticmethod
-    def is_valid(player, game):
-        return player.money >= game.pot.minimum_to_bet(player) > 0
+    def is_valid(player, round_):
+        return player.money >= round_.pot.minimum_to_bet(player) > 0
 
     def apply(self):
-        self.player.money -= self.game.pot.amount_to_call(self.player)
+        self.player.money -= self.round.pot.amount_to_call(self.player)
 
 
 class NotEnoughMoneyException(Exception):
@@ -127,11 +127,14 @@ class BasePlayer(object):
         self.bet(amount)
         return amount
 
-    def interact(self, game):
+    def interact(self, round_):
         raise NotImplementedError("Interact is not implemented on the Player's base class")
 
-    def is_betting(self, game):
-        return len(game.active_players) > 1 and (self.first_bet or (not self.is_folded(game) and self.money > 0))
+    def is_folded(self, round_):
+        return round_.is_folded(self)
+
+    def is_betting(self, round_):
+        return len(round_.active_players) > 1 and (self.first_bet or (not self.is_folded(round_) and self.money > 0))
 
     def possible_hands(self, community_cards):
         return [Hand.get_hand(cards) for cards in combinations(community_cards + self.pocket, r=5)]
@@ -144,34 +147,34 @@ class BasePlayer(object):
         best_hand = max(self.possible_hands(community_cards))
         return best_hand
 
-    def available_actions(self, game):
-        return list(filter(lambda x: x.is_valid(self, game), [Check, Call, Bet, Fold]))
+    def available_actions(self, round_):
+        return list(filter(lambda x: x.is_valid(self, round_), [Check, Call, Bet, Fold]))
 
-    def choose_action_message(self, game):
+    def choose_action_message(self, round_):
         return os.linesep.join([
             "%s, please choose an action:" % self.name
         ] + ['. '.join(
             [str(index), action_name.__name__]
-        ) for index, action_name in enumerate(self.available_actions(game))])
+        ) for index, action_name in enumerate(self.available_actions(round_))])
 
 
 class HumanPlayer(BasePlayer):
 
-    def interact(self, game):
+    def interact(self, round_):
         self.first_bet = False
 
         action = None
         while action is None:
             try:
-                available_actions = self.available_actions(game)
-                action = available_actions[int(input(self.choose_action_message(game)))]
+                available_actions = self.available_actions(round_)
+                action = available_actions[int(input(self.choose_action_message(round_)))]
 
                 while isinstance(action, AmountableAction):
                     # choose action amount (example: bet, 50)
                     while 0 > amount > self.money:
                         amount = int(input("How much?"))
-                    return action(self, game, amount)
-                return action(self, game)
+                    return action(self, round_, amount)
+                return action(self, round_)
             except ValueError:
                 pass
             except IndexError:
